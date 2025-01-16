c4971c576ba168c9bd8ca40078e1b18843219ea0
Velo Octavio <146362342+Voyager-Ov@users.noreply.github.com>
2025-01-15 22:33:24 -0300

    cambio la estructura de mi aplicacion web con los componentes
    basicamente agrego componentes como el headder el footer y el body en donde en cada uno tienenlas funciones que cada uno necesita

--- App_angular/src/app/app.component.ts
+++ App_angular/src/app/app.component.ts
@@ -1,83 +1,19 @@
 import { Component } from '@angular/core';
-import { NgIf } from '@angular/common';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-
+import { HeaderComponent } from './header/header.component'; // Importa el componente 'header'
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
+  imports: [ 
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: true,
-  imports: [NgIf, ReactiveFormsModule]
-  
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Alquimia Lavandería';
-  isPopupVisible = false; // Controla la visibilidad del popup
-  istagvisible = false
-
-
-  // Muestra el popup
-  openPopup() {
-    this.isPopupVisible = true;
-  }
-
-   // Oculta el popup con animación
-   closePopup() {
-    const overlay = document.querySelector('.popup-overlay');
-    const content = document.querySelector('.popup-content');
-    if (overlay && content) {
-      overlay.classList.add('hide');
-      content.classList.add('hide');
-      setTimeout(() => {
-        this.isPopupVisible = false;
-        overlay.classList.remove('hide');
-        content.classList.remove('hide');
-      }, 500); // Duración de la animación
-    } else {
-      this.isPopupVisible = false;
-    }
-  }
-
-  // Abre WhatsApp
-  // Abre WhatsApp con un mensaje predefinido
-openWhatsApp() {
-  const message = encodeURIComponent('Hola, me gustaría obtener más información sobre sus servicios.');
-  window.open(`https://wa.me/3883527777?text=${message}`, '_blank'); // Cambia el número por el tuyo
-}
-
-  // Abre Gmail
-  sendEmail() {
-    window.location.href = 'mailto:alquimialavanderia2025@gmail.com'; // Cambia el correo por el tuyo
-  }
-
-  // Abre Instagram
-  openInstagram() {
-    window.open('https://instagram.com/alquimialavanderia', '_blank'); // Cambia el usuario por el tuyo
-  }
-
-  scrollToAbout() {
-
-    document.getElementById('about')?.scrollIntoView({ behavior: 'smooth' });
-
-  }
-
-  scrollToServices() {
-
-    document.getElementById('services')?.scrollIntoView({ behavior: 'smooth' });
-
-  }
-
-  openclosetag() {
-    if (this.istagvisible === true) {
-        // abrir tag
-        this.istagvisible = false;
-    } else {
-        // cerrar tag
-        this.istagvisible = true;
-    }
-  }
-  
+  title = 'Alquimia Lavanderia';
 }
--- App_angular/src/app/app.module.ts
+++ App_angular/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 import { AppComponent } from './app.component'; // Import AppComponent
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    HttpClientModule // Add HttpClientModule to imports array
   ],
   providers: [],
 })
