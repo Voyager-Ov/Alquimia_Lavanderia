d3b94092d59fc0fe49ae48c58e358a534901a8ce
Velo Octavio <146362342+Voyager-Ov@users.noreply.github.com>
2025-01-16 18:10:00 -0300

    revierto cambis

--- App_angular/src/app/app.routes.ts
+++ App_angular/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   // Definir tus rutas aquí
